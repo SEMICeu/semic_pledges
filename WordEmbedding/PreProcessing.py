8ac06723524fde5c18bb851130c0de3a84805627
Ecaudron <emilien.caudron@pwc.com>
2023-04-07 11:05:08 +0200

    Correcting Code syntax + commenting

--- WordEmbedding/PreProcessing.py
+++ WordEmbedding/PreProcessing.py
@@ -33,10 +33,10 @@ PledgesDf = pd.read_csv(PledgesCsvPath, index_col=0) # Reading the csv file usin
 
 """ Defining common pre-processing functions """
 
-def first_clean(text): #Input - String or text you want to process
+def FirstClean(text): #Input - String or text you want to process
     return " ".join(text.split()).replace("_x000D_","") #Output - Same string without \n and _x000D_
 
-def preprocess(text): #Input - String or text you want to process
+def PreProcess(text): #Input - String or text you want to process
     text = text.lower() # Lowercase all the characters from the string
     text = text.strip() # Remove the leading and trailing whitespaces
     text = re.compile('<.*?>').sub('', text)
@@ -47,7 +47,7 @@ def preprocess(text): #Input - String or text you want to process
     text = re.sub(r'\s+', ' ', str(text).strip()) # Replacing "double, triple, etc" whitespaces by one
     return text #Output - Same string after all the transformations
 
-def stopword(string): #Input - String or text to process
+def StopWord(string): #Input - String or text to process
     a = [i for i in string.split() if i not in stopwords.words('english')] # Removing usual english stopwords from the string
     return ' '.join(a) #Output - Same string after all the transformations
 
@@ -55,7 +55,7 @@ def stopword(string): #Input - String or text to process
 # Initialize the lemmatizer
 wl = WordNetLemmatizer()
 # This is a helper function to map NTLK position tags
-def get_wordnet_pos(tag):
+def GetWordnetPos(tag):
     if tag.startswith('J'):
         return wordnet.ADJ
     elif tag.startswith('V'):
@@ -67,14 +67,14 @@ def get_wordnet_pos(tag):
     else:
         return wordnet.NOUN
 # Tokenize the sentence
-def lemmatizer(string):
-    word_pos_tags = nltk.pos_tag(nltk.word_tokenize(string))  # Get position tags
-    a = [wl.lemmatize(tag[0], get_wordnet_pos(tag[1])) for idx, tag in
-         enumerate(word_pos_tags)]  # Map the position tag and lemmatize the word/token
+def Lemmatizer(string):
+    WordPosTags = nltk.pos_tag(nltk.word_tokenize(string))  # Get position tags
+    a = [wl.lemmatize(tag[0], GetWordnetPos(tag[1])) for idx, tag in
+         enumerate(WordPosTags)]  # Map the position tag and lemmatize the word/token
     return " ".join(a)
 
 
-def preprocessing(text): # Combining all pre-processing steps
+def PreProcessing(text): # Combining all pre-processing steps
 
     global n
     n = n +1
@@ -83,14 +83,14 @@ def preprocessing(text): # Combining all pre-processing steps
     print("n is : ")
     print(n)
     print("length of the text is : ")
-    print(len(first_clean(text)))
-    return lemmatizer(stopword(preprocess(first_clean(text))))
+    print(len(FirstClean(text)))
+    return Lemmatizer(StopWord(PreProcess(FirstClean(text))))
 
 
 """ Main function """
 n=0
 print("begin pre-processing")
-PledgesDf['PreProcessedText'] = PledgesDf['Pledge'].apply(lambda x: preprocessing(x))
+PledgesDf['PreProcessedText'] = PledgesDf['Pledge'].apply(lambda x: PreProcessing(x))
 print("end pre-processing\n")
 
 print(PledgesDf.head())
@@ -98,21 +98,23 @@ print(PledgesDf.head())
 """ Visualisation """
 
 # Building a wordcloud for visualizing most frequent words
-cloud=WordCloud(colormap="ocean_r",width=600,height=400, background_color="#e5e5e5").generate(PledgesDf['PreProcessedText'].str.cat(sep=' '))
-fig=plt.figure(figsize=(13,18))
-plt.axis("off")
+cloud=WordCloud(colormap="ocean_r",width=600,height=400, background_color="#e5e5e5").generate(PledgesDf['PreProcessedText'].str.cat(sep=' ')) # Setting color for the map, background + defining dimensions
+fig=plt.figure(figsize=(13,18)) # Size of the figure
+plt.axis("off") # Removing the axis
 plt.imshow(cloud,interpolation='bilinear')
-plt.show()
+plt.show() 
 
 # Most frequent words in the pledges after pre-processing
-plt.style.use('ggplot')
-plt.figure(figsize=(14,6))
-freq=pd.Series(" ".join(PledgesDf['PreProcessedText']).split()).value_counts()[:30]
-freq.plot(kind="bar", color = "orangered")
-plt.title("30 most frequent words",size=20)
+plt.style.use('ggplot')  # Setting up the style
+plt.figure(figsize=(14,6)) # Setting up the size of the figure
+freq=pd.Series(" ".join(PledgesDf['PreProcessedText']).split()).value_counts()[:30] # Defining the series to plot, i.e., the 30 most frequent words
+freq.plot(kind="bar", color = "orangered") # Choosing the plot type (bar) and colore (orangered)
+plt.title("30 most frequent words",size=20) # Title
 plt.show()
 
-# Outputting the pre-processed files
+
+""" Outputing the pre-process data """
+
 PreProcessedDataPath = str(DirPpath.absolute()) + "\semic_pledges\PreProcessedData.csv"
 PledgesDf.to_csv(PreProcessedDataPath)
 
