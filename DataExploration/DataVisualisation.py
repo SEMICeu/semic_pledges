a54dd88e2c6dbe750f511422cafcd7a868f30717
Ecaudron <emilien.caudron@pwc.com>
2023-04-07 10:41:15 +0200

    Formatting Visuals

--- DataExploration/DataVisualisation.py
+++ DataExploration/DataVisualisation.py
@@ -4,6 +4,7 @@ import os # For finding source files
 from pathlib import Path
 
 import pandas as pd # For data handling
+import numpy as np
 import string # For handling of textual data
 
 import matplotlib.pyplot as plt # For data visualization
@@ -25,9 +26,12 @@ print(PledgeDf.head())
 
 """ Plotting some insights about the different topics and pledges """
 
-hist = PledgeDf["Topic"].plot(kind = 'hist', title = 'Count of Pledges per Topic', bins = 28, rwidth = 0.8) # Histogram of the number of pledge per topic
-hist.plot()
+sns.set_style("dark") # Building a catplot of the number of pledges per topic
+graph = sns.catplot(data=PledgeDf, x="Topic", kind="count", height = 3, aspect = 2.9, palette = "ocean")
+#plt.title("Frequency showing number of pledges assigned in different topics", size = 20, y = 0.8)
+plt.axhline(y=np.mean(np.array(PledgeDf.groupby("Topic").size())), color='blue', ls='--', lw=2.5) # Adding a mean line
 plt.show()
+
 
 # Defining a new column containing the number of words per pledge
 LengthList = [] # List that will contain the number of words of each pledge
@@ -35,8 +39,16 @@ for pledge in PledgeDf["Pledge"]: # Looping over the different pledges from the 
     LengthList.append(len(pledge.split())) # Appending the number of words to the pledge
 PledgeDf["Length"] = LengthList
 
-plt.boxplot(PledgeDf["Length"]) # Plotting the dispersion of words across the different pledges (looking for outliers)
+
+plt.figure(figsize=(8,6))
+sns.set_style("dark") 
+graph = sns.boxplot(data=PledgeDf["Length"], palette = "ocean") # Plotting the dispersion of word count across the different pledges (looking for outliers)
+plt.title("Boxplot of Pledges' Length", size = 20)
 plt.show()
+
+
 sns.boxplot(x = PledgeDf["Topic"], y = PledgeDf["Length"]) # Boxplots of length by topic
+plt.title("Boxplot of Pledges' Length by Topic", size = 20)
 plt.show()
+
 
